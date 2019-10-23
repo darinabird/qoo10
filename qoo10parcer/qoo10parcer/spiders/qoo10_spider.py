818b234043920f1677f144cf312ccb436c8720e4
Daria Butakhina <darinabird46722@gmail.com>
2019-10-23 19:25:24 +0300

    update2

--- qoo10parcer/qoo10parcer/spiders/qoo10_spider.py
+++ qoo10parcer/qoo10parcer/spiders/qoo10_spider.py
@@ -4,23 +4,28 @@ import scrapy
 
 class Qoo10SpiderSpider(scrapy.Spider):
     name = 'qoo10'
+    query = input('Put search query: ')
+    start_urls = [
+        'https://www.qoo10.sg/s/' + query + '?keyword=' + query + '&keyword_auto_change=',
+        'https://www.qoo10.sg/gmkt.inc/Search/SearchAjaxAppend.aspx?search_type=SearchQooBoRecommendItems'
+        '&search_keyword=' + query + '&shipto=RU&___cache_expire___=1571824461283',
+    ]
 
-    def start_requests(self):
-        urls = [
-            'https://www.qoo10.sg/s/EYELASH?keyword=eyelash&keyword_auto_change=',
-            # 'https://www.qoo10.sg/s/?search_option=tt&gdlc_cd=&gdmc_cd=&gdsc_cd=&keyword_hist=eyelash&delivery_group_no=&bundle_policy=&bundle_delivery=&keyword=&sortType=RANK_POINT_FEMALE&dispType=LIST&flt_pri_idx=&filterDelivery=NNNNNANNNN&search_global_yn=&basis=&shipFromNation=&shipto=RU&brandnm=&SearchNationCode=&is_research_yn=Y&hid_keyword=eyelash&quick_delivery_yn=&qprime_yn=&shipping_avg_dt=&video_goods_yn=&coupon_filter_no=&gd_type=&drugs_type=&relation_group_no=&multiple_ship_from_yn=Y&priceMin=&priceMax=&category_specific_kw_nos=&curPage=2&pageSize=60&partial=off&brandno=#anchor_detail_top'
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        items = []
-        for block in response.css('#search_result_item_list tr'):
-            items.append({
-                'description': block.css('a[data-type=goods_url]::attr(title)').get(),
-                'image_link': block.css('a[data-type=goods_url] img::attr(src)').get(),
-                'price': block.css('.prc strong::text').get(),
-            })
-        print('--- START CODE ---')
-        print(items)
-        print('--- END CODE ---')
+        if response.css('#search_result_item_list tr'):
+            for block in response.css('#search_result_item_list tr'):
+                yield {
+                        'description': block.css('a[data-type=goods_url]::attr(title)').get(),
+                        'image_link': block.css('a[data-type=goods_url] img::attr(src)').get(),
+                        'price': block.css('.prc strong::text').get(),
+                    }
+                continue
+
+        elif response.css('.slide'):
+            for block in response.css('.slide'):
+                yield{
+                    'description': block.css('.tt::text').get(),
+                    'image_link': block.css('.thmb img::text').get(),
+                    'price': block.css('.prc strong::text').get(),
+                }
