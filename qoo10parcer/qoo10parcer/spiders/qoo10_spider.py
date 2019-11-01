334ae3788ba08bbd2eabe4f682cbe1786dbb8cef
Daria Butakhina <darinabird46722@gmail.com>
2019-11-01 14:19:23 +0300

    done with pagination

--- qoo10parcer/qoo10parcer/spiders/qoo10_spider.py
+++ qoo10parcer/qoo10parcer/spiders/qoo10_spider.py
@@ -5,14 +5,10 @@ import scrapy
 class Qoo10SpiderSpider(scrapy.Spider):
     name = 'qoo10'
     query = input('Put search query: ')
+    page_num = 2  # input('Put page number: ')
+
     start_urls = [
-        'https://www.qoo10.sg/s/' + query + '?keyword=' + query + '&keyword_auto_change=',
-        'https://www.qoo10.sg/s/?search_option=tt&gdlc_cd=&gdmc_cd=&gdsc_cd=&keyword_hist=' + query +
-        '&delivery_group_no=&bundle_policy=&bundle_delivery=&keyword=&sortType=RANK_POINT_FEMALE&dispType=LIST&flt'
-        '_pri_idx=&filterDelivery=NNNNNANNNN&search_global_yn=&basis=&shipFromNation=&shipto=RU&brandnm=&SearchNation'
-        'Code=&is_research_yn=Y&hid_keyword=' + query + '&quick_delivery_yn=&qprime_yn=&shipping_avg_dt=&video_goods_'
-        'yn=&coupon_filter_no=&gd_type=&drugs_type=&relation_group_no=&multiple_ship_from_yn=Y&priceMin=&priceMax=&'
-        'category_specific_kw_nos=&curPage=2&pageSize=60&partial=off&brandno=',
+        'https://www.qoo10.sg/s/' + query + '?keyword=' + query + '&keyword_auto_change=&curPage=1',
     ]
 
 
@@ -26,10 +22,15 @@ class Qoo10SpiderSpider(scrapy.Spider):
                     }
                 continue
 
-        elif response.css('.slide'):
-            for block in response.css('.slide'):
-                yield{
-                    'description': block.css('.tt::text').get(),
-                    'image_link': block.css('.thmb img::text').get(),
-                    'price': block.css('.prc strong::text').get(),
-                }
+        for block in response.css('#search_result_item_list'):
+            yield{
+                'description': block.css('.sbj a::text').get(),
+                'image_link': block.css('.thmb img::text').get(),
+                'price': block.css('#search_result_item_list img::text').get(),
+            }
+
+        next_page = 'https://www.qoo10.sg/s/' + Qoo10SpiderSpider.query + '?keyword=' + Qoo10SpiderSpider.query\
+                    + '&keyword_auto_change=&curPage=' + str(Qoo10SpiderSpider.page_num) + ''
+        if Qoo10SpiderSpider.page_num < 10:
+            Qoo10SpiderSpider.page_num += 1
+            yield scrapy.Request(next_page, callback=self.parse)
