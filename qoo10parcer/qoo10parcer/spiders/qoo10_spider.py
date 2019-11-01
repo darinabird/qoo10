ca2e4c0f0ffed752ed1829f5e16093e8b7f36030
Daria Butakhina <darinabird46722@gmail.com>
2019-11-01 14:32:14 +0300

    added page_num user input and parsing till next_page is None

--- qoo10parcer/qoo10parcer/spiders/qoo10_spider.py
+++ qoo10parcer/qoo10parcer/spiders/qoo10_spider.py
@@ -5,32 +5,31 @@ import scrapy
 class Qoo10SpiderSpider(scrapy.Spider):
     name = 'qoo10'
     query = input('Put search query: ')
-    page_num = 2  # input('Put page number: ')
+    page_num = input('Put page number: ')
 
     start_urls = [
         'https://www.qoo10.sg/s/' + query + '?keyword=' + query + '&keyword_auto_change=&curPage=1',
     ]
 
-
     def parse(self, response):
         if response.css('#search_result_item_list tr'):
             for block in response.css('#search_result_item_list tr'):
                 yield {
-                        'description': block.css('a[data-type=goods_url]::attr(title)').get(),
-                        'image_link': block.css('a[data-type=goods_url] img::attr(src)').get(),
-                        'price': block.css('.prc strong::text').get(),
-                    }
+                    'description': block.css('a[data-type=goods_url]::attr(title)').get(),
+                    'image_link': block.css('a[data-type=goods_url] img::attr(src)').get(),
+                    'price': block.css('.prc strong::text').get(),
+                }
                 continue
 
         for block in response.css('#search_result_item_list'):
-            yield{
+            yield {
                 'description': block.css('.sbj a::text').get(),
                 'image_link': block.css('.thmb img::text').get(),
                 'price': block.css('#search_result_item_list img::text').get(),
             }
 
-        next_page = 'https://www.qoo10.sg/s/' + Qoo10SpiderSpider.query + '?keyword=' + Qoo10SpiderSpider.query\
+        next_page = 'https://www.qoo10.sg/s/' + Qoo10SpiderSpider.query + '?keyword=' + Qoo10SpiderSpider.query \
                     + '&keyword_auto_change=&curPage=' + str(Qoo10SpiderSpider.page_num) + ''
-        if Qoo10SpiderSpider.page_num < 10:
+        if next_page is not None:
             Qoo10SpiderSpider.page_num += 1
             yield scrapy.Request(next_page, callback=self.parse)
